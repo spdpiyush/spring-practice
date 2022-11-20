f99f5021cf2e6ec9da474c22131aa425d27efdb5
spdpiyush <piyushchamp.pkp@gmail.com>
2022-11-20 11:53:16 +0530

    config changes

--- src/test/java/com/piyush/springpractice/SpringPracticeApplicationTests.java
+++ src/test/java/com/piyush/springpractice/SpringPracticeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringPracticeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
